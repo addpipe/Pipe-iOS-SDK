dec828d706d3f4da66686e10a1ad7988cf282008
Octavian <octavian@nusofthq.com>
2017-12-05 11:40:42 +0200

    Correct path for uploading video files to our new infrastructure

--- Release/Pipe.framework/Headers/PipeRecorder.h
+++ Release/Pipe.framework/Headers/PipeRecorder.h
@@ -3,7 +3,7 @@
 //  Pipe
 //
 //  Created by Remus on 08/03/16.
-//  Copyright © 2016 nusofthq. All rights reserved.
+//  Copyright © 2016 Pipe Services SRL. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
